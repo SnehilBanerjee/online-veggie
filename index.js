9c405608d9d6a629ccd5e7464b48b880a009d724
Snehil Banerjee <snehilbanerji@live.com>
2023-01-22 13:05:26 +0530

    ADD basic CRUD operations for veggies

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+var app = express(); app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+let items = ["tomato", "potato", "broccoli", "Ginger"]
+app.post("/items", (req, res, next) => {
+    items.push(req.body.item)
+    res.json({ status: 200, message: `Added items ${req.body.item}` });
+});
+app.get("/items", (req, res, next) => {
+    res.json({ status: 200, items: items });
+});
+app.put("/items", (req, res, next) => {
+    items = req.body.items;
+    res.json({ status: 200, items: items });
+});
